c8e7d1f8858881985ea70834b2a22b46a748a2b9
dev6isss <dev6@isss.bz>
2026-08-31 17:45:04 +0530

    added my sql data base

--- myapp/views.py
+++ myapp/views.py
@@ -3,14 +3,24 @@ from django.http import HttpResponse
 from django.urls import reverse
 import logging
 
+
+heello = [
+    {"id": 1, "name": "John", "age": 30},
+    {"id": 2, "name": "Jane", "age": 25},
+    {"id": 3, "name": "Bob", "age": 40},
+]
+
 # Create your views here.
 def index(request):
     logger = logging.getLogger('Testing')
     logger.debug('summa oru debug message')
-    return HttpResponse("hello buddy")
+    return render(request, "index.html", {"posts": heello})
 
 def post(request, id):
-    return HttpResponse(f"Post ID {id}")
+    data = next((item for item in heello if item["id"] == int(id)), None)
+    logger = logging.getLogger('Testing')
+    logger.debug(f"the post variable is {data}")
+    return render(request, "post.html", {"post" : data})
 
 def firstpage(request):
     return render(request, 'firstpage.html')
